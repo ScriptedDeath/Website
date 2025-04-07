d5f7776d45c5f9272101be9cd5168cacb09bb8e9
ScriptedDeath <157943842+ScriptedDeath@users.noreply.github.com>
2025-04-08 00:16:32 +1200

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,8 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  reactStrictMode: false,  // Disable React Strict Mode
+  // Other config options here, such as images, i18n, etc.
+};
+
+export default nextConfig;
